75e219bce3c7f5919038d6a58c84531d54fdd8d9
Jan Fässler <jan@faessler.be>
2017-04-04 10:39:08 +0200

    styles

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
 // EUROMILLIONS
 // NUMBER
+var emArrNr = []
+while(emArrNr.length < 5){
+    var randomnumber = Math.ceil(Math.random()*50);
+    if(emArrNr.indexOf(randomnumber) > -1) continue;
+    emArrNr[emArrNr.length] = randomnumber;
+}
 var emnumbers = document.getElementsByClassName("em__numbers__nr");
 for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = Math.floor(Math.random() * 50) + 1;
+	var randomNr = emArrNr[i];
 	emnumbers[i].innerHTML = randomNr;
 }
 
 // STARS
+var emArrStar = []
+while(emArrStar.length < 2){
+    var randomnumber = Math.ceil(Math.random()*12);
+    if(emArrStar.indexOf(randomnumber) > -1) continue;
+    emArrStar[emArrStar.length] = randomnumber;
+}
 var emnumbers = document.getElementsByClassName("em__stars__wrap__nr");
 for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = Math.floor(Math.random() * 12) + 1;
+	var randomNr = emArrStar[i];
+	emnumbers[i].innerHTML = randomNr;
+}
+
+
+
+// SWISS LOTTO
+// NUMBER
+var slArrNr = []
+while(slArrNr.length < 6){
+    var randomnumber = Math.ceil(Math.random()*42);
+    if(slArrNr.indexOf(randomnumber) > -1) continue;
+    slArrNr[slArrNr.length] = randomnumber;
+}
+var emnumbers = document.getElementsByClassName("sl__numbers__nr");
+for(var i = 0; i < emnumbers.length; i++){
+	var randomNr = slArrNr[i];
+	emnumbers[i].innerHTML = randomNr;
+}
+
+// STARS
+var slArrExtra = []
+while(slArrExtra.length < 1){
+    var randomnumber = Math.ceil(Math.random()*12);
+    if(slArrExtra.indexOf(randomnumber) > -1) continue;
+    slArrExtra[slArrExtra.length] = randomnumber;
+}
+var emnumbers = document.getElementsByClassName("sl__extra__nr");
+for(var i = 0; i < emnumbers.length; i++){
+	var randomNr = slArrExtra[i];
 	emnumbers[i].innerHTML = randomNr;
 }
