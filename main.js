88f44f940510dea4c8aba0473605d1f75f3513cb
Jan Fässler <jan@faessler.be>
2017-04-04 12:05:31 +0200

    reshuffle

--- main.js
+++ main.js
@@ -1,83 +1,86 @@
-// CREATE SHUFFLE FUNCTION
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-}
+function shuffle() {
+	// CREATE SHUFFLE FUNCTION
+	function shuffle(a) {
+	    var j, x, i;
+	    for (i = a.length; i; i--) {
+	        j = Math.floor(Math.random() * i);
+	        x = a[i - 1];
+	        a[i - 1] = a[j];
+	        a[j] = x;
+	    }
+	}
 
-// EUROMILLIONS
-// NUMBER
-var numbers = [];
-for (var i = 0; i < 50; i++) {
-	numbers[i] = i+1;
-}
-shuffle(numbers);
+	// EUROMILLIONS
+	// NUMBER
+	var numbers = [];
+	for (var i = 0; i < 50; i++) {
+		numbers[i] = i+1;
+	}
+	shuffle(numbers);
 
-var emNumbers = document.getElementsByClassName("em__numbers__nr");
-var selectedNumbers = [];
-for (var i = 0; i < emNumbers.length; i++) {
-	selectedNumbers[i] = numbers[i];
-}
-selectedNumbers.sort(function(a, b){return a - b});
-for (var i = 0; i < selectedNumbers.length; i++) {
-	emNumbers[i].innerHTML = selectedNumbers[i];
-}
-
-
-// STARS
-var numbers = [];
-for (var i = 0; i < 12; i++) {
-	numbers[i] = i+1;
-}
-shuffle(numbers);
-
-var emStars = document.getElementsByClassName("em__stars__wrap__nr");
-var selectedNumbers = [];
-for (var i = 0; i < emStars.length; i++) {
-	selectedNumbers[i] = numbers[i];
-}
-selectedNumbers.sort(function(a, b){return a - b});
-for (var i = 0; i < selectedNumbers.length; i++) {
-	emStars[i].innerHTML = selectedNumbers[i];
-}
+	var emNumbers = document.getElementsByClassName("em__numbers__nr");
+	var selectedNumbers = [];
+	for (var i = 0; i < emNumbers.length; i++) {
+		selectedNumbers[i] = numbers[i];
+	}
+	selectedNumbers.sort(function(a, b){return a - b});
+	for (var i = 0; i < selectedNumbers.length; i++) {
+		emNumbers[i].innerHTML = selectedNumbers[i];
+	}
 
 
-// SWISS LOTTO
-// NUMBER
-var numbers = [];
-for (var i = 0; i < 42; i++) {
-	numbers[i] = i+1;
-}
-shuffle(numbers);
+	// STARS
+	var numbers = [];
+	for (var i = 0; i < 12; i++) {
+		numbers[i] = i+1;
+	}
+	shuffle(numbers);
 
-var slNumbers = document.getElementsByClassName("sl__numbers__nr");
-var selectedNumbers = [];
-for (var i = 0; i < slNumbers.length; i++) {
-	selectedNumbers[i] = numbers[i];
-}
-selectedNumbers.sort(function(a, b){return a - b});
-for (var i = 0; i < selectedNumbers.length; i++) {
-	slNumbers[i].innerHTML = selectedNumbers[i];
-}
+	var emStars = document.getElementsByClassName("em__stars__wrap__nr");
+	var selectedNumbers = [];
+	for (var i = 0; i < emStars.length; i++) {
+		selectedNumbers[i] = numbers[i];
+	}
+	selectedNumbers.sort(function(a, b){return a - b});
+	for (var i = 0; i < selectedNumbers.length; i++) {
+		emStars[i].innerHTML = selectedNumbers[i];
+	}
 
 
-// EXTRA NUMBER
-var numbers = [];
-for (var i = 0; i < 6; i++) {
-	numbers[i] = i+1;
-}
-shuffle(numbers);
+	// SWISS LOTTO
+	// NUMBER
+	var numbers = [];
+	for (var i = 0; i < 42; i++) {
+		numbers[i] = i+1;
+	}
+	shuffle(numbers);
 
-var slExtraNumber = document.getElementsByClassName("sl__extra__nr");
-var selectedNumbers = [];
-for (var i = 0; i < slExtraNumber.length; i++) {
-	selectedNumbers[i] = numbers[i];
+	var slNumbers = document.getElementsByClassName("sl__numbers__nr");
+	var selectedNumbers = [];
+	for (var i = 0; i < slNumbers.length; i++) {
+		selectedNumbers[i] = numbers[i];
+	}
+	selectedNumbers.sort(function(a, b){return a - b});
+	for (var i = 0; i < selectedNumbers.length; i++) {
+		slNumbers[i].innerHTML = selectedNumbers[i];
+	}
+
+
+	// EXTRA NUMBER
+	var numbers = [];
+	for (var i = 0; i < 6; i++) {
+		numbers[i] = i+1;
+	}
+	shuffle(numbers);
+
+	var slExtraNumber = document.getElementsByClassName("sl__extra__nr");
+	var selectedNumbers = [];
+	for (var i = 0; i < slExtraNumber.length; i++) {
+		selectedNumbers[i] = numbers[i];
+	}
+	selectedNumbers.sort(function(a, b){return a - b});
+	for (var i = 0; i < selectedNumbers.length; i++) {
+		slExtraNumber[i].innerHTML = selectedNumbers[i];
+	}
 }
-selectedNumbers.sort(function(a, b){return a - b});
-for (var i = 0; i < selectedNumbers.length; i++) {
-	slExtraNumber[i].innerHTML = selectedNumbers[i];
-}
+shuffle();
