b39fc392b400edd85988b92560c229fffc506dba
Jan Fässler <jan@faessler.be>
2017-04-04 11:15:37 +0200

    make it much cleaner :D

--- main.js
+++ main.js
@@ -1,55 +1,74 @@
-// EUROMILLIONS
-// NUMBER
-var emArrNr = []
-while(emArrNr.length < 5){
-    var randomnumber = Math.ceil(Math.random()*50);
-    if(emArrNr.indexOf(randomnumber) > -1) continue;
-    emArrNr[emArrNr.length] = randomnumber;
-}
-var emnumbers = document.getElementsByClassName("em__numbers__nr");
-for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = emArrNr[i];
-	emnumbers[i].innerHTML = randomNr;
+// CREATE SHUFFLE FUNCTION
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
 }
 
-// STARS
-var emArrStar = []
-while(emArrStar.length < 2){
-    var randomnumber = Math.ceil(Math.random()*12);
-    if(emArrStar.indexOf(randomnumber) > -1) continue;
-    emArrStar[emArrStar.length] = randomnumber;
+// EUROMILLIONS
+// NUMBER
+var numbers = [];
+for (var i = 0; i < 50; i++) {
+	numbers[i] = i+1;
 }
-var emnumbers = document.getElementsByClassName("em__stars__wrap__nr");
-for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = emArrStar[i];
-	emnumbers[i].innerHTML = randomNr;
+shuffle(numbers);
+
+var emNumbers = document.getElementsByClassName("em__numbers__nr");
+var selectedNumbers = [];
+for (var i = 0; i < emNumbers.length; i++) {
+	selectedNumbers[i] = numbers[i];
+	emNumbers[i].innerHTML = selectedNumbers[i];
+}
+
+
+
+// STARS
+var numbers = [];
+for (var i = 0; i < 12; i++) {
+	numbers[i] = i+1;
+}
+shuffle(numbers);
+
+var emStars = document.getElementsByClassName("em__stars__wrap__nr");
+var selectedNumbers = [];
+for (var i = 0; i < emStars.length; i++) {
+	selectedNumbers[i] = numbers[i];
+	emStars[i].innerHTML = selectedNumbers[i];
 }
 
 
 
 // SWISS LOTTO
 // NUMBER
-var slArrNr = []
-while(slArrNr.length < 6){
-    var randomnumber = Math.ceil(Math.random()*42);
-    if(slArrNr.indexOf(randomnumber) > -1) continue;
-    slArrNr[slArrNr.length] = randomnumber;
+var numbers = [];
+for (var i = 0; i < 42; i++) {
+	numbers[i] = i+1;
 }
-var emnumbers = document.getElementsByClassName("sl__numbers__nr");
-for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = slArrNr[i];
-	emnumbers[i].innerHTML = randomNr;
+shuffle(numbers);
+
+var slNumbers = document.getElementsByClassName("sl__numbers__nr");
+var selectedNumbers = [];
+for (var i = 0; i < slNumbers.length; i++) {
+	selectedNumbers[i] = numbers[i];
+	slNumbers[i].innerHTML = selectedNumbers[i];
 }
 
+
+
 // STARS
-var slArrExtra = []
-while(slArrExtra.length < 1){
-    var randomnumber = Math.ceil(Math.random()*12);
-    if(slArrExtra.indexOf(randomnumber) > -1) continue;
-    slArrExtra[slArrExtra.length] = randomnumber;
+var numbers = [];
+for (var i = 0; i < 12; i++) {
+	numbers[i] = i+1;
 }
-var emnumbers = document.getElementsByClassName("sl__extra__nr");
-for(var i = 0; i < emnumbers.length; i++){
-	var randomNr = slArrExtra[i];
-	emnumbers[i].innerHTML = randomNr;
+shuffle(numbers);
+
+var slExtraNumber = document.getElementsByClassName("sl__extra__nr");
+var selectedNumbers = [];
+for (var i = 0; i < slExtraNumber.length; i++) {
+	selectedNumbers[i] = numbers[i];
+	slExtraNumber[i].innerHTML = selectedNumbers[i];
 }
